4675019847a6f58b321522b55425b49b6dc42d16
rushindrasinha <gaape@live.com>
2015-09-01 00:38:12 +0530

    Add Week_2/Day_1

---
+++ week_2/day_1/exercise/js/app.js
@@ -0,0 +1,78 @@
+// // This first function is to get the users input ||RS||
+// function getInput() {
+//     console.log("Please choose either 'rock', 'paper', or 'scissors'.");
+//     return prompt();
+// }
+//
+// // This function is to generate a random pick for the computer based on a 33.33% chance or R , P or S ||RS||
+// function randomPlay() {
+//     var randomNumber = Math.random();
+//     if (randomNumber < 0.33) {
+//         return "rock";
+//     } else if (randomNumber < 0.66) {
+//         return "paper";
+//     } else {
+//         return "scissors";
+//     }
+// }
+//
+// // This function gets and returns the players move ||RS||
+// function getPlayerMove(move) {
+//     return move || getInput();
+// }
+//
+// // This function gets and returns the computers move ||RS||
+// function getComputerMove(move) {
+//     return move || randomPlay();
+// }
+//
+// // This function tracks and compares both player and computer input to return the winner ||RS||
+// function getWinner(playerMove,computerMove) {
+//     var win;
+//
+//     if ((playerMove === 'rock' && computerMove === 'paper')
+//         || (playerMove === 'paper' && computerMove === 'scissors')
+//         || (playerMove === 'scissors' && computerMove === 'rock')) {
+//         win = 'computer';
+//     }
+//     else if ((playerMove === 'rock' && computerMove === 'scissors')
+//             || (playerMove === 'paper' && computerMove === 'rock')
+//             || (playerMove === 'scissors' && computerMove === 'paper')) {
+//     win = 'player';
+//     }
+//     else if (playerMove === computerMove) {
+//         win = 'tie';
+//     }
+//
+//     console.log("\nYou chose " + playerMove + " and your opponent chose " + computerMove + " so clearly the winner is ");
+//     return win;
+// }
+//
+// function playToFive() {
+//     console.log("Let's play Rock, Paper, Scissors\n");
+//     var playerWins = 0;
+//     var computerWins = 0;
+//     var result;
+//
+//     while (playerWins < 5 && computerWins < 5) {
+//         result = getWinner(getPlayerMove(), getComputerMove());
+//
+//     if (result === 'player') {
+//         playerWins++;
+//         console.log('You!');
+//     }
+//     else if (result === 'computer') {
+//         computerWins++;
+//         console.log('Computer!');
+//     }
+//     else {
+//         console.log('Tied!');
+//     }
+//
+//     console.log("\nScoreboard\n You ::  " + playerWins + " & Computer :: " + computerWins + "\n");
+//
+//     }
+//     console.log("\n The Final Score is " + playerWins + " :: " + computerWins);
+// }
+//
+// playToFive();
---
+++ week_2/day_1/exercise/js/controller.js
@@ -0,0 +1,19 @@
+// var input = parseInt(prompt("Enter the highest number"));
+// var randomNumber = Math.floor(Math.random() * input);
+// console.log(randomNumber);
+//
+// var answer = parseInt(prompt("Guess The Number"));
+//
+// // while (answer !== randomNumber) {
+//   if ((answer) > randomNumber ) {
+//     console.log("Too high");
+//   } else if ((answer) < randomNumber ) {
+//     console.log("Too low");
+//     answer;
+//   } else if ((answer) === randomNumber ) {
+//       console.log("You Win");
+//   }  else {
+//     console.log("wah??");
+//     answer;
+//   }
+// // }
